b3796e4fca4ec30df4f0f2acdd3dd9f9d125903f
JayaKaviya <kavyamanz@gmail.com>
2025-09-16 14:04:01 +0530

    closure

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,10 +1,22 @@
+// CLOSURE 1
 function x(){
     for (var i=1;i<=5;i++){ 
     
         function close(x) {
-         setTimeout(function(){console.log(x);}, x*1000)  
+        //  setTimeout(function(){console.log(x);}, x*1000)  
         }  
         close(i);
     }
 }
-x();
+x(); 
+
+// CLOSURE 2
+function outer(){ 
+    function inner(){
+        console.log(count);
+    } 
+    var count=1; 
+    return inner;
+} 
+
+outer()();
