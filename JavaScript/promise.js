31eb2a32bacfd3fb0b4dd56b34804c53cb5388c1
JayaKaviya <kavyamanz@gmail.com>
2025-09-25 20:23:18 +0530

    Promise creation & consumption

--- JavaScript/promise.js
+++ JavaScript/promise.js
@@ -17,5 +17,58 @@ user.then(res => res.json())
 // Response 
 // Data  {login: 'JayaKaviya', id: 109425248, 
 //     node_id: 'U_kgDOBoWyYA', 
-//     avatar_url: 'https://avatars.githubusercontent.com/u/109425248?v=4', gravatar_id: '', …} 
+//     avatar_url: 'https://avatars.githubusercontent.com/u/109425248?v=4', gravatar_id: '', …}  
+
+
+//Example 2
+
+const cart=['shoes','pants','shirts']
+
+//Consumer
+const promise=createOrder(cart)
+
+//promise chaining
+promise
+    .then(function(x){
+        console.log("ORDER ID ",x);
+        return x;
+    })
+    .then(function(){
+        return proceedToPayment(x);
+    }) 
+    .then(function(res){
+     console.log("PAYMENT STATUS",res);
+    })
+    .catch(function(err){
+        console.log("ERROR", err.message);
+    })
+
+
+//Producer
+function createOrder(cart){ 
+
+    var pr=new Promise(function(resolve,reject){
+        if(!ValidityState(cart)){
+            var err=new Error("Cart is not valid"); 
+            reject(err);
+        }
+        else{          
+            const orderID="12345";
+            resolve(orderID);
+        }
+    }) 
+    return pr;
+
+} 
+function ValidityState(cart){
+    return true;
+    //  return false;
+} 
+
+function proceedToPayment(orderID){
+    return  new Promise(function(res,rej){
+        res("Payment successful");
+    })
+}
+
 
