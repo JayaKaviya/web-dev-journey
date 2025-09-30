44869154e4734d09d3a031e61bcf0a7ddd919b6a
JayaKaviya <kavyamanz@gmail.com>
2025-09-30 12:46:30 +0530

    async-await

--- JavaScript/async.js
+++ JavaScript/async.js
@@ -7,14 +7,47 @@
 // console.log(dataPromise);
 // dataPromise.then(res=>console.log(res)); 
 
-const p=new Promise((resolve,reject)=>{
-    resolve("Promise resolved");
-})
-async function x(){
-    return p;
+
+//ex 2
+// const p=new Promise((resolve,reject)=>{
+//     resolve("Promise resolved");
+// })
+// async function x(){
+//     return p;
+// } 
+// const dataPromise=x(); 
+// console.log(dataPromise);
+// dataPromise.then(res=>console.log(res));  
+
+
+//ex 3
+
+const pr1=new Promise((resolve,reject)=>{
+     setTimeout(()=>resolve("Promise1 resolved after 5s"),5000);
+}) 
+const pr2=new Promise((resolve,reject)=>{
+   setTimeout(()=>resolve("Promise2 resolved after 10 s"),10000);
+}) 
+console.log("1");
+console.log("1");
+console.log("1");
+console.log("1");
+console.log("1");
+console.log("1");
+console.log("1");
+console.log("1");
+async function handlepromise() {
+
+    const res=await pr1; 
+    console.log("Namaste 1");
+    console.log(res); 
+
+    const res2=await pr2; 
+    console.log("Namaste 2");
+    console.log(res2);
+    
 } 
-const dataPromise=x(); 
-console.log(dataPromise);
-dataPromise.then(res=>console.log(res)); 
+handlepromise();
+
 
 
